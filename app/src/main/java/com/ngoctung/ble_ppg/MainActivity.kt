3bc1d0df004af0e34c6f6650807b8162af3a74bb
Tung Nguyen <tungnguyenngoc@itrvn.com>
2023-06-22 20:24:03 +0700

    update: init project android

---
+++ app/src/main/java/com/ngoctung/ble_ppg/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ngoctung.ble_ppg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
